b4091adf46f32546032ac13a4e7fa01d876349d7
Leandro-Barretoo <officialdevmi@gmail.com>
2021-11-30 10:30:41 -0800

    Updated weight_kg using savepoint, rollback and commit

--- data.sql
+++ data.sql
@@ -100,3 +100,21 @@ DELETE FROM animals;
 ROLLBACK;
 
 SELECT * FROM animals;
+
+BEGIN;
+
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+ROLLBACK TO SP1;
+
+UPDATE animals
+SET weight_kg = weight_kg * -1
+WHERE weight_kg < 0;
+
+COMMIT;
