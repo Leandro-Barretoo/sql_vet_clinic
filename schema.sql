41e63e43b1e5cfe9059b199742e27d9b4f932590
Leandro-Barretoo <officialdevmi@gmail.com>
2021-12-01 07:35:21 -0800

    Modify animals table to reference new tables

--- schema.sql
+++ schema.sql
@@ -23,3 +23,30 @@ CREATE TABLE species(
   id     INT GENERATED ALWAYS AS IDENTITY,
   name   VARCHAR(255)
 );
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD species_id INT;
+
+ALTER TABLE species
+ADD PRIMARY KEY(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT species_fk
+FOREIGN KEY(species_id)
+REFERENCES species(id)
+ON DELETE CASCADE;
+
+ALTER TABLE animals
+ADD owner_id INT;
+
+ALTER TABLE owners
+ADD PRIMARY KEY(id);
+
+ALTER TABLE animals
+ADD CONSTRAINT owner_fk
+FOREIGN KEY(owner_id)
+REFERENCES owners(id)
+ON DELETE CASCADE;
