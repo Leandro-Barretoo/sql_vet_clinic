0b0490a549d45f46f9d0fd8b9ec9517df18298ec
Leandro-Barretoo <officialdevmi@gmail.com>
2021-12-02 09:24:02 -0800

    Add specializations and visits join tables

--- schema.sql
+++ schema.sql
@@ -50,3 +50,33 @@ ADD CONSTRAINT owner_fk
 FOREIGN KEY(owner_id)
 REFERENCES owners(id)
 ON DELETE CASCADE;
+
+CREATE TABLE vets(
+  id                 INT GENERATED ALWAYS AS IDENTITY,
+  name               VARCHAR(255),
+  age                INT,
+  date_of_graduation DATE,
+  PRIMARY KEY(id) 
+);
+
+CREATE TABLE specializations(
+  vets_id INT,
+  species_id INT,
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vets_id)
+      REFERENCES vets(id),
+  CONSTRAINT fk_species
+    FOREIGN KEY(species_id)
+      REFERENCES species(id)
+);
+
+CREATE TABLE visits(
+  vets_id INT,
+  animals_id INT,
+  CONSTRAINT fk_vets
+    FOREIGN KEY(vets_id)
+      REFERENCES vets(id),
+  CONSTRAINT fk_animals
+    FOREIGN KEY(animals_id)
+      REFERENCES animals(id)
+);
